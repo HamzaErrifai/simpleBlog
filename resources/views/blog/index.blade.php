122bee2a55c655af59ed2e322d1aa7e872b4f5a0
Hamza <hamzaerrifai@hotmail.com>
2022-03-13 00:34:58 +0100

    now posts are rendered dynamicaly

--- resources/views/blog/index.blade.php
+++ resources/views/blog/index.blade.php
@@ -8,23 +8,24 @@
 
         </div>
     </div>
-    <div class="sm:grid grid-cols-2 gap-20 w-4/5 mx-auto py-15 border-b border-gray-200">
-        <div class="">
-            <img src="https://images.pexels.com/photos/18105/pexels-photo.jpg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
-                width="700" alt="">
+    @foreach ($posts as $post)
+        <div class="sm:grid grid-cols-2 gap-20 w-4/5 mx-auto py-15 border-b border-gray-200">
+            <div class="">
+                <img src="{{ $post->image_path }}" width="700" alt="">
+            </div>
+            <div class="">
+                <h2 class="text-gray-700 font-bold text-5xl pb-4">{{ $post->title }}</h2>
+                <span class="text-gray-500">
+                    By <span class="font-bold italic text-gray-800">{{ $post->user->name }}</span>
+                </span>, {{ date('jS M Y', strtotime($post->updated_at)) }}
+                <p class="text-xl text-gray-700 pt-8 pb-10 leading-8 font-light">
+                    {{ $post->description }}
+                </p>
+                <a href="/blog/{{ $post->slug }}"
+                    class="uppercase bg-blue-500 text-gray-100 text-lg font-extrabold py-4 px-8 rounded-3xl">
+                    keep reading
+                </a>
+            </div>
         </div>
-        <div class="">
-            <h2 class="text-gray-700 font-bold text-5xl pb-4">Learn how to write Laravel Code</h2>
-            <span class="text-gray-500">
-                By <span class="font-bold italic text-gray-800">Hamza</span>
-            </span>, 1 day ago
-            <p class="text-xl text-gray-700 pt-8 pb-10 leading-8 font-light">
-                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Saepe possimus mollitia quam sed nihil illo esse
-                non fugiat hic, fugit, assumenda modi odit commodi? Dignissimos reprehenderit deserunt saepe ex officiis!
-            </p>
-            <a href="" class="uppercase bg-blue-500 text-gray-100 text-lg font-extrabold py-4 px-8 rounded-3xl">
-              keep reading
-            </a>
-        </div>
-    </div>
+    @endforeach
 @endsection
