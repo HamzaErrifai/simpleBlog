a03884181f4b1894ece4e4d124df17764f4e87cc
Hamza <hamzaerrifai@hotmail.com>
2022-03-13 02:20:08 +0100

    basic project is complete

--- resources/views/blog/index.blade.php
+++ resources/views/blog/index.blade.php
@@ -8,10 +8,27 @@
 
         </div>
     </div>
+    @if (session()->has('message'))
+        <div class="w-4/5 m-auto mt-10 pl-2">
+            <p class="w-1/6 mb-4 text-gray-50 bg-green-500 rounded-2xl px-6 py-4">
+                {{ session()->get('message') }}
+            </p>
+        </div>
+    @endif
+
+    @if (Auth::check())
+        <div class="pt-15 w-4/5 m-auto">
+            <a class="bg-blue-500 uppercase bg-transparent text-gray-100 text-xs font-extrabold py-3 px-5 rounded-3xl"
+                href="/blog/create">
+                Create a post
+            </a>
+        </div>
+    @endif
+
     @foreach ($posts as $post)
         <div class="sm:grid grid-cols-2 gap-20 w-4/5 mx-auto py-15 border-b border-gray-200">
             <div class="">
-                <img src="{{ $post->image_path }}" width="700" alt="">
+                <img src="{{ asset($post->image_path) }}" width="700" alt="">
             </div>
             <div class="">
                 <h2 class="text-gray-700 font-bold text-5xl pb-4">{{ $post->title }}</h2>
@@ -25,6 +42,19 @@
                     class="uppercase bg-blue-500 text-gray-100 text-lg font-extrabold py-4 px-8 rounded-3xl">
                     keep reading
                 </a>
+                @if (isset(Auth::user()->id) && Auth::user()->id == $post->user_id)
+                    <span class="float-right">
+                        <a href="/blog/{{ $post->slug }}/edit"
+                            class="text-gray-700 italic hover:text-gray-900 pb-1 border-b-2">Edit</a>
+                    </span>
+                    <span class="float-right">
+                        <form action="/blog/{{ $post->slug }}" method="POST">
+                            @csrf
+                            @method('delete')
+                            <button class="text-red-500 pr-3" type="submit">Delete</button>
+                        </form>
+                    </span>
+                @endif
             </div>
         </div>
     @endforeach
